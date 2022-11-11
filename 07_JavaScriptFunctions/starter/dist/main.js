d303845ea275c528dd5a99ad9a15b0b3bd54406e
AndySmithQA <111853853+AndySmithQA@users.noreply.github.com>
2022-11-11 08:56:16 +0000

    Delete 07_JavaScriptFunctions directory

--- 07_JavaScriptFunctions/starter/dist/main.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-console.log('If you can see me, it worked');
-/******/ })()
-;
-//# sourceMappingURL=main.js.map
